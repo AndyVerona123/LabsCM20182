0a88ff1b1a0808aafbbca9ca4b1a4d7534aeb5c4
AndyVerona123 <andres.carrascal@udea.edu.co>
2018-08-25 14:42:31 -0500

    Se agregaron las opciones de solo landscape, se agregó una imagen de fondo para el splash screen y se implementó la pantalla de bienvenida con reedireccionaminto a el main acivity.

---
+++ Lab1-UI/src/main/java/co/edu/udea/compumovil/gr05_20182/lab1/SplashScreen.java
@@ -0,0 +1,22 @@
+package co.edu.udea.compumovil.gr05_20182.lab1;
+
+import android.content.Intent;
+import android.os.Handler;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class SplashScreen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash_screen);
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intenet = new Intent(SplashScreen.this,MainActivity.class);
+                startActivity(intenet);
+            }
+        },3000);
+    }
+}
