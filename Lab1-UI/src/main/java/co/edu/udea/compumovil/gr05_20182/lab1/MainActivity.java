e524c62f17eb812ddc95c9e847eec43031f15c49
AndyVerona123 <andres.carrascal@udea.edu.co>
2018-08-27 01:41:19 -0500

    Avance de la app

--- Lab1-UI/src/main/java/co/edu/udea/compumovil/gr05_20182/lab1/MainActivity.java
+++ Lab1-UI/src/main/java/co/edu/udea/compumovil/gr05_20182/lab1/MainActivity.java
@@ -1,7 +1,10 @@
 package co.edu.udea.compumovil.gr05_20182.lab1;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageButton;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +12,13 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ImageButton imageButtonbutton =  findViewById(R.id.buttom_food);
+        imageButtonbutton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent imageButtonbutton = new Intent(MainActivity.this,FoodActivity.class);
+                startActivity(imageButtonbutton);
+            }
+        });
     }
 }
